26bd3fc24048ea23557471f8abdceda09a46772c
Tang Man <189658646@qq.com>
2019-06-11 17:26:04 +0800

    [spike][mxgraph] make graphedtior work in angular

--- src/app/mxgraph-page/mxgraph/mxgraph.component.ts
+++ src/app/mxgraph-page/mxgraph/mxgraph.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+// import '../../../util/www/js/Init';
+// import "../../../util/www/js/Init.js";
+// import "../../../util/www/deflate/pako.min.js";
+// import "../../../util/www/deflate/base64.js";
+// import "../../../util/www/jscolor/jscolor.js";
+// import "../../../util/www/sanitizer/sanitizer.min.js";
+// import "../../../../node_modules/mxgraph/javascript/mxClient.js";
+// import "../../../util/www/js/EditorUi.js";
+// import "../../../util/www/js/Editor.js";
+// import "../../../util/www/js/Sidebar.js";
+// import "../../../util/www/js/Graph.js";
+// import "../../../util/www/js/Format.js";
+// import "../../../util/www/js/Shapes.js";
+// import "../../../util/www/js/Actions.js";
+// import "../../../util/www/js/Menus.js";
+// import "../../../util/www/js/Toolbar.js";
+// import "../../../util/www/js/Dialogs.js";
+import { run } from "./mxgraph-run.js";
+
+
 
 @Component({
   selector: 'app-mxgraph',
@@ -10,6 +30,10 @@ export class MxgraphComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    run();
   }
 
 }
